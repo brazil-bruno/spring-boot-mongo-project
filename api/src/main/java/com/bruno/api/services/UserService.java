1d280d0348d75c2ff4322a69efb5f345184078d0
brazil-bruno <bruno.brazil2022@gmail.com>
2024-04-23 16:20:14 -0300

    method new user

--- api/src/main/java/com/bruno/api/services/UserService.java
+++ api/src/main/java/com/bruno/api/services/UserService.java
@@ -1,5 +1,6 @@
 package com.bruno.api.services;
 
+import com.bruno.api.dto.UserDTO;
 import com.bruno.api.entities.User;
 import com.bruno.api.repositories.UserRepository;
 import com.bruno.api.services.exceptions.ObjectNotFoundException;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = userRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public User insert(User obj) {
+        return userRepository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
