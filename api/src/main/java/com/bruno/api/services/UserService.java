1b5b800981536ccb32a8d25d6ea33f9b34b6f884
brazil-bruno <bruno.brazil2022@gmail.com>
2024-04-23 16:13:59 -0300

    exceptions and find user by id

--- api/src/main/java/com/bruno/api/services/UserService.java
+++ api/src/main/java/com/bruno/api/services/UserService.java
@@ -2,10 +2,12 @@ package com.bruno.api.services;
 
 import com.bruno.api.entities.User;
 import com.bruno.api.repositories.UserRepository;
+import com.bruno.api.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
 }
