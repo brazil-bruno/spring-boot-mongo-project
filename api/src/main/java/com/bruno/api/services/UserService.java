355751ae9ab7ef939924d81caa6e8bf4693c371d
brazil-bruno <bruno.brazil2022@gmail.com>
2024-04-23 14:26:55 -0300

    mongodb connection

---
+++ api/src/main/java/com/bruno/api/services/UserService.java
@@ -0,0 +1,19 @@
+package com.bruno.api.services;
+
+import com.bruno.api.entities.User;
+import com.bruno.api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+}
