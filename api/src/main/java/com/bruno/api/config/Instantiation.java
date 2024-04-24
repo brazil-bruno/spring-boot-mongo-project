25842e4a60a30c86eea3df2de53b0ae3fe853322
brazil-bruno <bruno.brazil2022@gmail.com>
2024-04-24 12:15:22 -0300

    comments association to posts

--- api/src/main/java/com/bruno/api/config/Instantiation.java
+++ api/src/main/java/com/bruno/api/config/Instantiation.java
@@ -1,6 +1,7 @@
 package com.bruno.api.config;
 
 import com.bruno.api.dto.AuthorDTO;
+import com.bruno.api.dto.CommentDTO;
 import com.bruno.api.entities.Post;
 import com.bruno.api.entities.User;
 import com.bruno.api.repositories.PostRepository;
@@ -29,6 +30,7 @@ public class Instantiation implements CommandLineRunner {
         sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
@@ -39,6 +41,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2023"), "My Trip", "Trip to Australia", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2023"), "Hello world", "Good day", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Good travel", sdf.parse("05/11/2020"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Enjoy", sdf.parse("05/11/2020"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Have a nice day", sdf.parse("05/11/2020"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
