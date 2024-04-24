8a1898cafe74d8c083d05707f8fce4ef8753e352
brazil-bruno <bruno.brazil2022@gmail.com>
2024-04-24 10:25:49 -0300

    entity Post created

--- api/src/main/java/com/bruno/api/config/Instantiation.java
+++ api/src/main/java/com/bruno/api/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.bruno.api.config;
 
+import com.bruno.api.entities.Post;
 import com.bruno.api.entities.User;
+import com.bruno.api.repositories.PostRepository;
 import com.bruno.api.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
@@ -14,15 +18,26 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         userRepository.deleteAll();
+
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+        Post post1 = new Post(null, sdf.parse("21/03/2023"), "My Trip", "Trip to Australia", maria);
+        Post post2 = new Post(null, sdf.parse("23/03/2023"), "Hello world", "Good day", maria);
+
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
